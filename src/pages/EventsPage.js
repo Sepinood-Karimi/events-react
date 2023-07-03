1c8fe1f3e6905f20e673e7f885103c7dcea90e8c
Sepinoud Karimi <sepinoodkarimi@gmail.com>
2023-07-03 13:13:46 +0330

    refactor : use json() for cleaning code

--- src/pages/EventsPage.js
+++ src/pages/EventsPage.js
@@ -1,5 +1,5 @@
 import EventsList from "../components/EventsList";
-import { useLoaderData } from "react-router-dom";
+import { json, useLoaderData } from "react-router-dom";
 
 const EventsPage = () => {
   const response = useLoaderData();
@@ -13,9 +13,7 @@ export const eventsLoader = async () => {
   if (response.ok) {
     return response;
   } else {
-    throw new Response(JSON.stringify({ message: "Could not fetch events" }), {
-      status: 500,
-    });
+    throw json({ message: "Could not fetch events" }, { status: 500 });
   }
 };
 export default EventsPage;
